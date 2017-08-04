4d08cc0ddfe740d9740078823e0a36d4e8c6a3fc
vandopinho <evandrojrx0@gmail.com>
2017-08-03 23:28:23 -0300

    First Commit

---
+++ buffer.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int bufferSize = 256;
+char *buffer[bufferSize];
+int bufferControl=0;
+int bufferPos=0;
+
+FILE *file;
+file = fopen("sample.sql", "a+");
+
+void clearBuffer(){
+        buffer = [];
+        bufferControl = 0;
+        bufferPos = 0;
+    }
+
+void fullBuffer(){
+        char nextChar = fgetc(file);
+        while (bufferControl < bufferSize){
+            buffer[bufferControl] = nextChar;
+            nextChar = fgetc(file);
+          }
+}
+char getChar(){
+        bufferPos = bufferPos + 1;
+        return buffer[bufferPos-1];
+}
+void resetBuffer(){
+        bufferPos = bufferPos - 1;
+}
+void refillBuffer(){
+        while(True):
+            if(bufferPos == bufferSize):
+                clearBuffer();
+                fullBuffer();
+}
+
+fclose(file);
---
+++ grammar.c
@@ -0,0 +1,23 @@
+#define ERR    = -1000
+
+#define INSERT = 1000
+#define UPDATE = 1001
+#define DELETE = 1002
+#define INTO   = 1003
+#define SET    = 1004
+#define WHERE  = 1005
+#define FROM   = 1006
+#define VALUES = 1007
+
+#define EOL    = 1008
+#define OPEN   = 1009
+#define CLOSE  = 1010
+#define COMMA  = 1011
+#define EQUALS = 1012
+
+#define ID     = 1013
+#define STRING = 1014
+#define NUMBER = 1015
+
+#define NNULL  = 1016
+#define SELECT = 1017
